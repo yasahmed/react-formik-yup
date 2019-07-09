66daaf17e586c11962483d73d5fbea841e2ac4cc
mac <mac@Virus1001.local.tld>
2019-07-09 11:39:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MyForm } from './BasicV2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div class="jumbotron">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <h3>React + Formik - Form Validation</h3>
+                    <MyForm/>
+                </div>
+            </div>
+        </div>
+    </div>
+      
     </div>
   );
 }
